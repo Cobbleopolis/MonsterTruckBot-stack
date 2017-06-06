1bb47d811c887fab260a65cd14789a29a309acab
Cobbleopolis <cobbleopolis@gmail.com>
2017-06-06 18:33:35 -0400

    Added a version command

---
+++ common/src/main/scala/com/cobble/bot/common/api/commands/VersionCommand.scala
@@ -0,0 +1,9 @@
+package com.cobble.bot.common.api.commands
+
+trait VersionCommand {
+
+    val name: String = "version"
+
+    val modOnly: Boolean = false
+
+}
--- discord/src/main/scala/com/cobble/bot/discord/DiscordCommandRegistry.scala
+++ discord/src/main/scala/com/cobble/bot/discord/DiscordCommandRegistry.scala
@@ -1,14 +1,17 @@
 package com.cobble.bot.discord
 
 import com.cobble.bot.discord.api.DiscordCommand
-import com.cobble.bot.discord.commands.DiscordPingCommand
+import com.cobble.bot.discord.commands.{DiscordPingCommand, DiscordVersionCommand}
 
 object DiscordCommandRegistry {
 
     val pingCommand: DiscordPingCommand = new DiscordPingCommand()
 
+    val versionCommand: DiscordVersionCommand = new DiscordVersionCommand()
+
     val commandList: Map[String, DiscordCommand] = Map(
-        pingCommand.name -> pingCommand
+        pingCommand.name -> pingCommand,
+        versionCommand.name -> versionCommand
     )
 
 }
